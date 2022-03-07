f502151d9ad6ba72d1ab0cb7aed86ad95d208ff8
Tensiorr <tesiorowski.marek@gmail.com>
2022-03-07 23:19:07 +0100

    Change database and update heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,15 +80,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
+DATABASES = {
+    'default': db_from_env
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
