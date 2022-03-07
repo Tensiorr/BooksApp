042707fd9cee68a8cab9f9aebb275365a137c63a
Tensiorr <tesiorowski.marek@gmail.com>
2022-03-07 22:36:05 +0100

    init heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -131,3 +134,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend')
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
